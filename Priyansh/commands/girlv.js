34c90935997565ea03990b7a7f0547d872233b5d
Ahmedk1107865 <updatep696@gmail.com>
2025-05-11 01:17:06 +0500

    Update girlv.js

--- Priyansh/commands/girlv.js
+++ Priyansh/commands/girlv.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["MR.AARYAN"];
+   var hi = ["MR.CHUZA"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
